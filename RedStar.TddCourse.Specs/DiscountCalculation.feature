2779146a441c397d6bc142e216a5fc58bc933c41
Peter Morlion <peter.morlion@gmail.com>
2018-02-20 12:58:28 +0100

    Add specflow

---
+++ RedStar.TddCourse.Specs/DiscountCalculation.feature
@@ -0,0 +1,10 @@
+﻿Feature: DiscountCalculation
+	In order to avoid atract more customers
+	As a sales representative
+	I want to calculate possible discounts
+
+Scenario: Birthday
+	Given A customer
+	And it is his birthday
+	When I calculate the discount
+	Then the result should be 40
---
+++ RedStar.TddCourse.Specs/DiscountCalculationSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentAssertions;
+using RedStar.TddCourse.Discounts;
+using RedStar.TddCourse.TestInfrastructure;
+using TechTalk.SpecFlow;
+
+namespace RedStar.TddCourse.Specs
+{
+    [Binding]
+    public class DiscountCalculationSteps
+    {
+        [Given(@"A customer")]
+        public void GivenACustomer()
+        {
+            ScenarioContext.Current["Customer"] = new Customer();
+        }
+        
+        [Given(@"it is his birthday")]
+        public void GivenItIsHisBirthday()
+        {
+            ((Customer) ScenarioContext.Current["Customer"]).Birthdate = DateTime.UtcNow;
+        }
+        
+        [When(@"I calculate the discount")]
+        public void WhenICalculateTheDiscount()
+        {
+            var discountCalculator = new DiscountCalculatorFactory().Create();
+            var customer = ((Customer) ScenarioContext.Current["Customer"]);
+            ScenarioContext.Current["Discount"] = discountCalculator.CalculateDiscount(customer);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int expectedDiscount)
+        {
+            ((double) ScenarioContext.Current["Discount"]).Should().Be(expectedDiscount);
+        }
+    }
+}
